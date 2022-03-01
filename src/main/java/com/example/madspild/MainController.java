9ec7cfdae16ed4b96ef445f72149307cc14d609d
JonasBaekbo <jonas.bryde.baekbo@live.dk>
2022-03-01 10:19:47 +0100

    Initial commit

---
+++ src/main/java/com/example/madspild/MainController.java
@@ -0,0 +1,20 @@
+package com.example.madspild;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+    @Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("title", "Forside - Madspild");
+        return "index";
+    }
+        @GetMapping("/privat")
+        public String privat(Model model){
+            model.addAttribute("title", "Til privatpersoner - Madspild");
+            return "about";
+        }
+}
